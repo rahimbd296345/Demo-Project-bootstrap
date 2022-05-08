e3465354460d43aef0cc0283442100e2b04a44cd
Abdur Rahim <www.shatotacorporation@gmail.com>
2022-05-08 22:58:03 +0600

    Add all

---
+++ js/functions.js
@@ -0,0 +1,37 @@
+jQuery(function($){
+    $('.good-slider').slick({
+        dots: false,
+        infinite: true,
+        arrows: false,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    infinite: true,
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                    infinite: true,
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    infinite: true,
+                }
+            }
+        ]
+    });
+});
